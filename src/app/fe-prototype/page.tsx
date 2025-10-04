285f2c078e43485e8196702b108686ed950ff7eb
Kauê Melo <kaue.cmelo@gmail.com>
2025-10-04 08:52:29 -0300

    Added FE Prototype page

---
+++ src/app/fe-prototype/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+export default function FEPrototype() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[16px] row-start-2 items-center sm:items-start">
+        <div className="container max-w-3xl">
+         Kauê
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://www.spaceappschallenge.org/2025/find-a-team/solaria4/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Team Solaria
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://www.spaceappschallenge.org/2025/challenges/embiggen-your-eyes/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          See full challenge description →
+        </a>
+      </footer>
+    </div>
+  );
+}
