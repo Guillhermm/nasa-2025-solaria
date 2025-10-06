7dd4d89ae2d17b6c9fe130adb571c5cd7567f3b6
Guillhermm <guillhermm_zenni@hotmail.com>
2025-10-05 22:25:09 -0300

    Update first page paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,17 +40,13 @@ export default function Home() {
 
   return (
     <Container>
-      <h1 className="font-mono mb-4">NASA 2025 Solaria</h1>
-      <h2 className="font-mono">Embiggen Your Eyes!</h2>
-      <div className="font-mono text-sm/6">
-        While your cell phone screen can display about three million pixels of
-        information and your eye can receive more than ten million pixels, NASA
-        images from space are even bigger! NASA’s space missions continue to
-        push the boundaries of what is technologically possible, providing
-        high-resolution images and videos of Earth, other planets, and space
-        with billions or even trillions of pixels. Your challenge is to create a
-        platform that allows users to zoom in and out on these massive image
-        datasets, label known features, and discover new patterns.
+      <h1 className="mb-4">NASA 2025 Solaria</h1>
+      <h2 className="">Explore the Universe in Unprecedented Detail</h2>
+      <div className="fs-5">
+        NASA{`\’`}s space missions produce some of the largest and most detailed
+        images ever captured, far beyond what your screen, or even your eyes,
+        can take in at once. With Solaria, you can explore, zoom,
+        compare, and analyze these ultra-high-resolution images in different ways.
       </div>
       <SectionDivider />
       <DeepViewer title="1. Deep Viewer" id="hubble-mosaic-high" />
